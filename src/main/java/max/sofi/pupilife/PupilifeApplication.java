c28949fb326170caac85951d6d853c71c77b0cf9
MaxiCarrillo <maxi11carrillo@gmail.com>
2023-07-22 21:02:00 -0300

    Proyecto

---
+++ src/main/java/max/sofi/pupilife/PupilifeApplication.java
@@ -0,0 +1,13 @@
+package max.sofi.pupilife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PupilifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PupilifeApplication.class, args);
+	}
+
+}
