a1f6734e61e5a19ffd9eea2017bc215c4d678fda
sofi <abrilyanez123@gmail.com>
2023-07-25 17:24:46 -0300

    receta

--- src/main/java/max/sofi/pupilife/controller/RecetaController.java
+++ src/main/java/max/sofi/pupilife/controller/RecetaController.java
@@ -1,10 +1,22 @@
 package max.sofi.pupilife.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import max.sofi.pupilife.entity.Receta;
+import max.sofi.pupilife.service.RecetaService;
 
 @Controller
 public class RecetaController {
+	@Autowired
+	private RecetaService recetaService;
 	
 	/**
 	 * Método para manejar la solicitud GET en la ruta "/receta".
@@ -16,4 +28,25 @@ public class RecetaController {
 	public String getReceta() {
 		return "receta";
 	}
+	
+	@GetMapping("/receta_nueva")
+	public String recetaNueva(Model model) {
+		model.addAttribute("receta", new Receta());
+		return "receta_nueva";
+	}
+	
+	@PostMapping("/receta_nueva")
+	public ModelAndView postRecetaNueva(@Validated @ModelAttribute("receta") Receta receta, BindingResult bindingResult ) {
+		if(bindingResult.hasErrors()) {
+			ModelAndView model = new ModelAndView("receta_nueva");
+			model.addObject("receta", receta);
+			return model;
+		}
+		recetaService.agregarReceta(receta);
+		ModelAndView model = new ModelAndView("redirect:/inicio");
+		return model;
+	}
+	
+	
+	
 }
