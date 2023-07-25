a96b943f2578d6c8767228ff541bd3b01c18b9e2
sofi <abrilyanez123@gmail.com>
2023-07-25 13:58:04 -0300

    anotacion service

--- src/main/java/max/sofi/pupilife/service/IngredienteService.java
+++ src/main/java/max/sofi/pupilife/service/IngredienteService.java
@@ -1,7 +1,10 @@
 package max.sofi.pupilife.service;
 
+import org.springframework.stereotype.Service;
+
 import max.sofi.pupilife.entity.Ingrediente;
 
+@Service
 public interface IngredienteService {
 	public void agregarIngrediente(Ingrediente ingrediente);
 	public void eliminarIngrediente(Long id);
--- src/main/java/max/sofi/pupilife/service/RecetaService.java
+++ src/main/java/max/sofi/pupilife/service/RecetaService.java
@@ -1,7 +1,10 @@
 package max.sofi.pupilife.service;
 
+import org.springframework.stereotype.Service;
+
 import max.sofi.pupilife.entity.Receta;
 
+@Service
 public interface RecetaService {
 	public void agregarReceta(Receta receta);
 	public void eliminarReceta(Long id);
