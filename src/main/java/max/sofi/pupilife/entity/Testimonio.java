8c2a383e6acf3f1bd1c9592853f1e684fd365d85
sofi <abrilyanez123@gmail.com>
2023-07-27 17:12:36 -0300

    entidad testimonio

---
+++ src/main/java/max/sofi/pupilife/entity/Testimonio.java
@@ -0,0 +1,78 @@
+package max.sofi.pupilife.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+
+public class Testimonio {
+	
+	private Long id;
+	private Usuario usuario;
+	private String comentario;
+	private LocalDate fecha;
+	private boolean estado;
+	
+	public Testimonio() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Testimonio(Long id, Usuario usuario, String comentario, LocalDate fecha, boolean estado) {
+		super();
+		this.id = id;
+		this.usuario = usuario;
+		this.comentario = comentario;
+		this.fecha = fecha;
+		this.estado = estado;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	
+	
+	
+	
+
+}
